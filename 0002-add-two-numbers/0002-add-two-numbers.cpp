aae96e6c44c422647b998939cf743225d4800a58
Ma <58978349+richard7714@users.noreply.github.com>
2023-04-12 16:02:10 +0900

    Time: 44 ms (34.76%), Space: 71.5 MB (56.07%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,52 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
-/**
- two non-negative int, non-empty linked list
- digit in reverse order
- add two num and return sum as linked list
- no leading zero, except 0
-*/
-
-/**
- 일의 자리부터 덧셈. 올림이 발생할 시 또 다른 int 변수에 기록
- 일의 자리부터 0이 나올경우 추가 iter 진행 X
-*/
-
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* pL1, ListNode* pL2) {
-        ListNode* pOutput = pL1;
-        for(;;)
-        {
-            if(!pL1->next && pL2)
-            {
-                pL1->next = pL2->next;
-                pL2->next = nullptr;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *list3 = new ListNode();
+        ListNode *l3 = list3;
+        ListNode* dummy = l3;
+        int carry = 0;
+        while(l1 != NULL || l2 != NULL || carry) {
+            int curSum = carry;
+            if(l1 != NULL) {
+                curSum += l1 -> val;
+                l1 = l1 -> next;
             }
-            if(pL2 != nullptr)
-                pL1->val += pL2->val;
-            if (pL1->val > 9){
-                if(pL1->next)
-                    pL1->next->val+=1;
-                else
-                    pL1->next = new ListNode(1);
-                pL1->val -= 10;
+            if(l2 != NULL) {
+                curSum += l2 -> val;
+                l2 = l2 -> next;
             }
-            if (!pL1->next)
-                break;
-            pL1 = pL1->next;
-            if(pL2 != nullptr)
-                pL2 = pL2->next;
+            ListNode *node = new ListNode(curSum % 10);
+            l3 -> next = node;
+            l3 = l3 -> next;
+            carry = curSum/10;
         }
-        return pOutput;
+        return dummy -> next;
     }
 };
