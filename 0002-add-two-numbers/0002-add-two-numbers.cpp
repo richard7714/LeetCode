bc455399d733b221f292e155d880be8f90645ad8
Ma <58978349+richard7714@users.noreply.github.com>
2023-04-12 16:02:34 +0900

    Time: 48 ms (20.94%), Space: 71.6 MB (14.10%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,25 +1,80 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *list3 = new ListNode();
-        ListNode *l3 = list3;
-        ListNode* dummy = l3;
-        int carry = 0;
-        while(l1 != NULL || l2 != NULL || carry) {
-            int curSum = carry;
-            if(l1 != NULL) {
-                curSum += l1 -> val;
-                l1 = l1 -> next;
-            }
-            if(l2 != NULL) {
-                curSum += l2 -> val;
-                l2 = l2 -> next;
-            }
-            ListNode *node = new ListNode(curSum % 10);
-            l3 -> next = node;
-            l3 = l3 -> next;
-            carry = curSum/10;
+       /* ListNode p1=NULL;
+        ListNode c1=l1;
+        ListNode t1=c1->next;
+        ListNode p2=NULL;
+        ListNode c2=l2;
+        ListNode t2=c2->next;
+        while(t1!=NULL)
+        {
+            c1->next=p1;
+            p1=c1;
+            c1=t1;
+            t1=t1->next;
         }
-        return dummy -> next;
+        while(t2!=NULL)
+        {
+            c2->next=p2;
+            p2=c2;
+            c2=t2;
+            t2=t2->next;
+        }
+        int con1=0;
+        int con2=0;
+        ListNode cur1=l1;
+        ListNode cur2=l2;
+        while(cur1!=NULL)
+        {
+            con1++;
+            cur1=cur1->next;
+        }
+        while(cur2!=NULL)
+        {
+            con2++;
+            cur2=cur2->next;
+        }
+        if(con1==con2)
+        {
+            while(con1--)
+            {
+                int k=c1->val+c2->val;
+                if(k>9)
+                {
+                   int s=k%10;
+                   ListNode *ans 
+                }
+            }
+        }*/
+        ListNode *ans=new ListNode();
+        ListNode *p1=l1;
+        ListNode *p2=l2;
+        ListNode *cur=ans;
+        int c=0;
+        int x,y;
+        while(p1!=NULL ||p2 !=NULL)
+        {
+            x=(p1!=NULL)?p1->val:0;
+            y=(p2!=NULL)?p2->val:0;
+            int sum=x+y+c;
+            c=sum/10;
+            cur->next=new ListNode(sum%10);
+            cur=cur->next;
+
+            if(p1!=NULL)
+            {
+                p1=p1->next;
+            }
+            if(p2!=NULL)
+            {
+                p2=p2->next;
+            }
+        }  
+        if(c>0)
+        {
+            cur->next=new ListNode(c);
+        } 
+        return ans->next;     
     }
 };
