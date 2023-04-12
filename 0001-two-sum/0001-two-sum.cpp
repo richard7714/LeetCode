4eb7ebcc333e112fa5204d86d71ef9904c33c39d
Ma <58978349+richard7714@users.noreply.github.com>
2023-04-12 13:18:20 +0900

    Time: 14 ms (67.85%), Space: 11.2 MB (13.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int len_nums = nums.size();
+        map<int,int> index_map;
         vector<int> output;
-        output.resize(2);
-        for (int idx1=0;idx1<len_nums;idx1++)
+        for (int idx = 0; idx<nums.size();idx++)
         {
-            int residual = target - nums[idx1];
-            vector<int> nums_2nd = nums;
-            nums_2nd.erase(nums_2nd.begin()+idx1);
-            for (int idx2=0;idx2<(len_nums-1);idx2++)
+            int residual = target - nums[idx];
+            if(index_map.find(residual) != index_map.end())
             {
-                if(nums_2nd[idx2]==residual)
-                {
-                    output[0] = idx1;
-                    output[1] = (idx2+1);
-                    return output;
-                }
+                output.push_back(index_map.find(residual)->second);
+                output.push_back(idx);
+                return output;
             }
+            index_map.insert({nums[idx],idx});
         }
         return output;
     }
-};
+}; 
